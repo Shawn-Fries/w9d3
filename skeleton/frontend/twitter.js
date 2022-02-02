7f08105c8ad60a7610be248b29bb843bcc7b8e05
Ethan Lam <ethanlam0@yahoo.com>
2022-02-02 14:17:51 -0500

    Add follow toggle to twitter.js

--- skeleton/frontend/twitter.js
+++ skeleton/frontend/twitter.js
@@ -1,1 +1,8 @@
-require('./follow_toggle.js')
+const FollowToggle = require('./follow_toggle.js')
+
+
+
+$(function () {
+    $("button.follow-toggle").each ((id, ele) => new FollowToggle(ele))
+});
+
