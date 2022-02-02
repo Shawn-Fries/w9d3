f45a0ca88db3e14fd31f60fafbcad0c2a4d700b9
Shawn-Fries <samfoley275@gmail.com>
2022-02-02 14:55:01 -0500

    working on render function

--- skeleton/frontend/follow_toggle.js
+++ skeleton/frontend/follow_toggle.js
@@ -2,6 +2,15 @@ function FollowToggle (el) {
     const $el = $('.follow-toggle');
     this.userId = $el.user-id;
     this.followState = $el.initial-follow-state;
+    render($el);
+}
+
+FollowToggle.prototype.render = function(el) {
+    if (el.followState) {
+        el.append(<p>"Follow!"</p>);
+    } else {
+        el.append(<p>"Unfollow!"</p>);
+    }
 }
 
 module.exports = FollowToggle;
